63ca484fb98735d9cd54bce6c85495fd8f3b25c3
nathnael19 <nathnaelnigussie19@gmail.com>
2025-10-02 13:54:21 +0300

    creating and registering Person model

--- products/models.py
+++ products/models.py
@@ -5,3 +5,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Person(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
