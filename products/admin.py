5ef9b55daa43e2c6007c2aba790fbb296bfaa9e6
nathnael19 <nathnaelnigussie19@gmail.com>
2025-10-02 14:16:54 +0300

    creating and registering Product model

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category,Person
+from .models import Category,Person,Product
 
 admin.site.register(Category)
 admin.site.register(Person)
+admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -16,3 +16,17 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    description = models.TextField(null=True,blank=True)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
